bfeaa35b5b81c80215564ecfb522772570754c11
Thiên Thiên <77221040+nameyoutime@users.noreply.github.com>
2021-05-13 11:58:41 +0700

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,13 +5,14 @@ const Database = require("./database");
 const { instance } = require("./database");
 const db = new Database();
 
+//key string to connect to db
 let connectionString =
   "mongodb+srv://admin:admin@cluster0.yhduf.mongodb.net/img?retryWrites=true&w=majority";
 
 async function main() {
   try {
+    //run server
     await instance.connectToDb(connectionString);
-    // app.use(cors());
     app.listen(config.PORT, config.HOST, () => {
       console.log(`server is running on ${config.HOST}:${config.PORT}`);
     });
